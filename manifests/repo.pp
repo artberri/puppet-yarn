7f41c780e0036fe8821dfec42f7a6101f7d4e751
Shane Drury <shane.drury@fanduel.com>
2017-02-08 12:13:09 +0000

    Use the less flakey yarnpkg gpg key

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -18,7 +18,7 @@ class yarn::repo (
           repos    => 'main',
           key      => {
             'id'     => '72ECF46A56B4AD39C907BBB71646B01B86E50310',
-            'server' => 'pgp.mit.edu',
+            'source' => 'https://dl.yarnpkg.com/debian/pubkey.gpg',
           },
         }
 
