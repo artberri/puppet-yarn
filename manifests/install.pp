827e40803553096d3c2eb2982adf288378c05eb9
Romain Tartiere <romain@blogreen.org>
2017-08-05 22:51:16 +0200

    Add some more attributes

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,16 @@
 define yarn::install (
   Boolean $production = true,
+  Optional[String] $group = undef,
+  Optional[String] $user = udef,
+  Optional[Integer] $timeout = undef,
 ) {
   exec { "yarn-install-${title}":
     command => "yarn install --production=${production}",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     cwd     => $title,
     unless  => 'yarn check',
+    user    => $user,
+    group   => $group,
+    timeout => $timeout,
   }
 }
