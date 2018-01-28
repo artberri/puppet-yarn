3790fbb556f8201e5aff319262136af1c1eedb06
Romuald Conty <neomilium@gmail.com>
2018-01-28 02:18:09 +0100

    Allow to specify environment while running yarn install

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,18 +2,20 @@ define yarn::install (
   Boolean $production = true,
   Optional[String] $group = undef,
   Optional[String] $user = undef,
+  Optional[Array] $environment = undef,
   Optional[Integer] $timeout = undef,
 ) {
   include yarn
 
   exec { "yarn-install-${title}":
-    command => "yarn install --production=${production}",
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-    cwd     => $title,
-    unless  => "yarn check --production=${production}",
-    user    => $user,
-    group   => $group,
-    timeout => $timeout,
+    command     => "yarn install --production=${production}",
+    path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+    cwd         => $title,
+    unless      => "yarn check --production=${production}",
+    user        => $user,
+    group       => $group,
+    environment => $environment,
+    timeout     => $timeout,
   }
   Anchor['yarn::end']
   -> Exec["yarn-install-${title}"]
